b997ee11934c2ee357bb476ddcc28845fde8586b
Harshit Garg <gargh2120@gmail.com>
2026-03-09 20:37:07 +0530

    type defined

---
+++ type/index.d.ts
@@ -0,0 +1,14 @@
+interface contactSchemaProp extends mongoose.Document {
+    name: string,
+    email: string,
+    subject: string,
+    message: string,
+    status: string,
+}
+interface FormProp  {
+    name: string,
+    email: string,
+    subject: string,
+    message: string,
+    status: string,
+}
