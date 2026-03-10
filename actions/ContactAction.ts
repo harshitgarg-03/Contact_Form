319648f6344695d07799828adc535844ade76068
Harshit Garg <gargh2120@gmail.com>
2026-03-10 10:55:03 +0530

    Server action functionality defines

---
+++ actions/ContactAction.ts
@@ -0,0 +1,111 @@
+"use server";
+
+import dbConnection from "@/lib/db";
+import { Contact } from "@/models/contact";
+import { revalidateTag, unstable_cache } from "next/cache";
+
+export const FormAction = async (formdata: FormData) => {
+  await dbConnection();
+
+  try {
+    const data = {
+      name: formdata.get("name") as string,
+      email: formdata.get("email") as string,
+      message: formdata.get("message") as string,
+      subject: formdata.get("subject") as string,
+    };
+
+    if (!data.name || !data.email || !data.message || !data.subject) {
+      return {
+        success: false,
+        message: "All fields are required!",
+        status: 404,
+      };
+    }
+    console.log("data is ", data);
+
+    const result = await Contact.create({
+      name: data.name,
+      email: data.email,
+      message: data.message,
+      subject: data.subject,
+    });
+    revalidateTag("contact-stats", "max");
+    await GetContactStats();
+    return {
+      success: true,
+      message: "message sent success!",
+      status: 200,
+      contactId: result._id.toString(),
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "server get failed",
+      status: 404,
+    };
+  }
+};
+
+export const getContacts = async () => {
+  await dbConnection();
+  try {
+    const contaclist = await Contact.find({}).sort({ createdAt: -1 }).lean();
+
+    if (!contaclist) {
+      console.log("no one contact contain in contact list");
+    }
+
+    return contaclist.map((contact) => ({
+      ...contact,
+      _id: contact._id.toString(),
+    }));
+
+  } catch (error: any) {
+    console.log("error to fetch contact list !", error);
+    
+    return [];
+  }
+};
+
+
+export const UpdateContacts = async (contactId: string, status: string) => {
+  await dbConnection();
+  try {
+    const contaclist = await Contact.findByIdAndUpdate(contactId, {status})
+
+    if (!contaclist) {
+      console.log("no one contact contain in contact list");
+    }
+    revalidateTag("contact-stats", "max")
+    await GetContactStats()
+
+    return {
+      success: true,
+      message: "update success!"
+    }
+
+  } catch (error: any) {
+    console.log("error to fetch contact list !", error);
+    return [];
+  }
+};
+
+const getCachedStats = unstable_cache(
+  async () => {
+    await dbConnection();
+
+    const total = await Contact.countDocuments();
+    const newCount = await Contact.countDocuments({ status: "new" });
+    const repliedCount = await Contact.countDocuments({ status: "replied" });
+    const readCount = await Contact.countDocuments({ status: "read" });
+
+    return { total, newCount, repliedCount, readCount };
+  },
+  ["contact-stats"],
+  { tags: ["contact-stats"] }
+);
+
+export const GetContactStats = async () => {
+  return getCachedStats();
+};
--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -1,107 +1,126 @@
-"use client";
-
+"use client"
 import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Label } from "./ui/label";
-// import { FormProp } from "@/next-env";
 import { FormAction } from "@/actions/ContactAction";
+import { useRouter } from "next/navigation";
 
 function contactForm() {
   const [isSubmit, setisSubmit] = useState<boolean>(false);
-  const [mesasge, setmessage] = useState<string>("");
-  
+  const [isGetContact, setisGetContact] = useState<boolean>(false);
+  const [message, setmessage] = useState<string>("");
+
+  const router = useRouter();
+
   async function onsubmit(formdata: FormData) {
     setisSubmit(true);
+    setmessage("message sent success 🎫");
 
     await FormAction(formdata);
-    // const formdatas = document.getElementById("conact-form");
-    // formdatas?.reset;
     setisSubmit(false);
   }
 
   return (
-    <Card className="max-w-xl mx-auto shadow-xl rounded-2xl border bg-white p-6 md:p-8">
-      <CardHeader className="text-center space-y-2 pb-6">
-        <CardTitle className="text-3xl font-semibold tracking-tight">
-          Contact Us
-        </CardTitle>
-        <p className="text-gray-500 text-sm">
-          We'd love to hear from you. Fill out the form below.
-        </p>
-      </CardHeader>
+    <>
+      <Card className="max-w-xl mx-auto shadow-xl rounded-2xl border bg-white p-6 md:p-8">
+        <CardHeader className="text-center space-y-2 pb-6">
+          <CardTitle className="text-3xl font-semibold tracking-tight">
+            Contact Us
+          </CardTitle>
+          <p className="text-gray-500 text-sm">
+            We'd love to hear from you. Fill out the form below.
+          </p>
+        </CardHeader>
 
-      <CardContent className="px-2">
-        <form id="conact-form" action={onsubmit} className="space-y-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        {message && (
+          <div>
+            <h3>{message}</h3>
+          </div>
+        )}
+
+        <CardContent className="px-2">
+          <form id="conact-form" action={onsubmit} className="space-y-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              <div className="space-y-2">
+                <Label htmlFor="name" className="text-sm font-medium">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Enter your name"
+                  className="py-2"
+                  required
+                  // onChange={(e) => setname(e.target.value)}
+                />
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="email" className="text-sm font-medium">
+                  Email
+                </Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  className="py-2"
+                  required
+                />
+              </div>
+            </div>
+
             <div className="space-y-2">
-              <Label htmlFor="name" className="text-sm font-medium">
-                Name
+              <Label htmlFor="subject" className="text-sm font-medium">
+                Subject
               </Label>
               <Input
-                id="name"
-                name="name"
+                id="subject"
+                name="subject"
                 type="text"
-                placeholder="Enter your name"
+                placeholder="Subject of your message"
                 className="py-2"
                 required
-                // onChange={(e) => setname(e.target.value)}
               />
             </div>
 
             <div className="space-y-2">
-              <Label htmlFor="email" className="text-sm font-medium">
-                Email
+              <Label htmlFor="message" className="text-sm font-medium">
+                Message
               </Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Enter your email"
-                className="py-2"
+              <Textarea
+                id="message"
+                name="message"
+                placeholder="Write your message..."
                 required
+                className="w-full min-h-32.5 rounded-md border px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-black"
               />
             </div>
-          </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="subject" className="text-sm font-medium">
-              Subject
-            </Label>
-            <Input
-              id="subject"
-              name="subject"
-              type="text"
-              placeholder="Subject of your message"
-              className="py-2"
-              required
-            />
-          </div>
-
-          <div className="space-y-2">
-            <Label htmlFor="message" className="text-sm font-medium">
-              Message
-            </Label>
-            <Textarea
-              id="message"
-              name="message"
-              placeholder="Write your message..."
-              required
-              className="w-full min-h-32.5 rounded-md border px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-black"
-            />
-          </div>
-
-          <Button
-            type="submit"
-            className="w-full py-2 text-base font-medium tracking-wide"
-          >
-            {isSubmit ? "Sending message..." : "Send Message"}
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
+            <Button
+              type="submit"
+              className="w-full py-2 text-base font-medium tracking-wide"
+            >
+              {isSubmit ? "Sending message..." : "Send Message"}
+            </Button>
+          </form>
+        </CardContent>
+        <Button
+          className="w-full py-2 text-base font-medium tracking-wide"
+          onClick={() => {
+            (setisGetContact(!isGetContact),
+              router.push("/getContacts"),
+              setisGetContact(!isGetContact));
+          }}
+        >
+          {isGetContact ? "Fetching contacts..." : "Contacts list"}
+        </Button>
+      </Card>
+    </>
   );
 }
 
